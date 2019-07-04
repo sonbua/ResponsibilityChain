f7fb20a0a4c90206a544aad81893f77edbc41b8c
Son Nguyen <dinh.son.nguyen@gmail.com>
2019-07-04 11:41:41 +0700

    - Move ActivatorServiceProvider to ResponsibilityChain.Interception.Tests project. It was not used in ResponsibilityChain. - Also remove ServiceProviderExtensions.

--- test/ResponsibilityChain.Interception.Tests/InterceptionTest.cs
+++ test/ResponsibilityChain.Interception.Tests/InterceptionTest.cs
@@ -287,5 +287,39 @@ namespace ResponsibilityChain.Interception.Tests
                 AddHandler(fallbackHandler);
             }
         }
+
+        /// <summary>
+        /// Represents a singleton service provider which uses <see cref="Activator"/> to create instance of given service type.
+        /// </summary>
+        private class ActivatorServiceProvider : IServiceProvider
+        {
+            static ActivatorServiceProvider()
+            {
+            }
+
+            private ActivatorServiceProvider()
+            {
+            }
+
+            /// <summary>
+            /// Singleton instance of this service provider.
+            /// </summary>
+            public static ActivatorServiceProvider Instance { get; } = new ActivatorServiceProvider();
+
+            /// <summary>
+            /// Uses <see cref="Activator"/> to create instance of the given <paramref name="serviceType"/>.
+            /// </summary>
+            /// <param name="serviceType"></param>
+            /// <returns></returns>
+            public object GetService(Type serviceType)
+            {
+                if (serviceType.IsAbstract)
+                {
+                    return null;
+                }
+
+                return Activator.CreateInstance(serviceType);
+            }
+        }
     }
 }
